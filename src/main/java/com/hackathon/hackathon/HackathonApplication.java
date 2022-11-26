d923ab4ee058445bde22081cdd8e870c6b7269d8
Marcin Jarczewski <marcin.jarc@gmail.com>
2022-11-26 02:49:23 +0100

    add build folder to gitignore

--- src/main/java/com/hackathon/hackathon/HackathonApplication.java
+++ src/main/java/com/hackathon/hackathon/HackathonApplication.java
@@ -1,9 +1,12 @@
 package com.hackathon.hackathon;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class HackathonApplication {
 
 	public static void main(String[] args) {
