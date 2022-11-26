cbda8ebc96d850b728f91b80c2775cf6c6021bf2
Marcin Jarczewski <marcin.jarc@gmail.com>
2022-11-26 03:15:26 +0100

    working demo

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,14 @@
 const startBtn = document.getElementById("start-btn");
 const form = document.getElementById("form");
+const parap = document.getElementsByTagName("p")[0];
 
-startBtn.addEventListener("click", () => {
+let text = fetch("https://dog.ceo/api/breeds/image/random")
+  .then((response) => response.json())
+  .then((response) => {
+    return response.message + " " + response.status;
+  });
+
+startBtn.addEventListener("click", async () => {
   console.log("dupa");
+  parap.innerHTML = await text;
 });
