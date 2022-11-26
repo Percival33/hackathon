a6328345ea94f74332189a9e726cad61b85f78dc
Wiktor Chmielewski <chmielewski.wiktor@gmail.com>
2022-11-26 03:47:12 +0100

    Added exception handling

--- src/main/java/com/hackathon/hackathon/controller/UserController.java
+++ src/main/java/com/hackathon/hackathon/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.hackathon.hackathon.controller;
 
+import com.hackathon.hackathon.model.Gender;
 import com.hackathon.hackathon.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/user")
@@ -19,5 +22,13 @@ public class UserController {
         Long uid = userService.generateUid();
 
         return ResponseEntity.ok(uid);
+    }
+
+    @PostMapping("/updateInfo")
+    public ResponseEntity<Void> updateInfo(@RequestHeader Long uid, @RequestParam String firstName,
+                                           @RequestParam String lastName, @RequestParam Gender gender) {
+        userService.updatePersonalData(uid, firstName, lastName, gender);
+
+        return ResponseEntity.ok(null);
     }
 }
