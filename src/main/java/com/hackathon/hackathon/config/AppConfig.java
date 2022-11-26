9657aeca2a1303c240a4d50c707ca17d8e1c6244
Wiktor Chmielewski <chmielewski.wiktor@gmail.com>
2022-11-26 04:07:50 +0100

    Changed idea

--- src/main/java/com/hackathon/hackathon/config/AppConfig.java
+++ src/main/java/com/hackathon/hackathon/config/AppConfig.java
@@ -1,10 +1,12 @@
 package com.hackathon.hackathon.config;
 
+import com.hackathon.hackathon.model.User;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
@@ -15,6 +17,11 @@ public class AppConfig {
         Server server = new Server();
         server.setUrl("");
         return new OpenAPI().servers(List.of(server));
+    }
+
+    @Bean
+    List<User> users() {
+        return new ArrayList<>();
     }
 
 }
